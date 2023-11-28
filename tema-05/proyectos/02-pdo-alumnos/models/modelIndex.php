6c08e78c83d8ba282f04acdc9b86f9b08930398c
Den <drodsan2708@g.educaand.es>
2023-11-28 10:51:42 +0100

    Actividad 5.2 finalizada

---
+++ tema-05/proyectos/02-pdo-alumnos/models/modelIndex.php
@@ -0,0 +1,14 @@
+<?php
+    /*
+        Modelo: modelIndex
+        Descripcion: crear un array con los detalles de los alumnos
+    */
+    setlocale(LC_MONETARY,"es_ES"); // Indicamos 
+
+    // Conecto con la base de datos
+    $conexion = new Alumnos();
+
+    // Extraigo los valores del alumno
+    $alumnos = $conexion->getAlumnos();
+
+?>
