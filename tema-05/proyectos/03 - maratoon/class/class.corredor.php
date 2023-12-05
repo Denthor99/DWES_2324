c1fde7a14cd56621c86a7443cfd990f89cfc2d3c
Denthor99 <darancuga@hotmail.com>
2023-12-05 09:31:38 +0100

    Inicio proyecto + inicio tema 6

---
+++ tema-05/proyectos/03 - maratoon/class/class.corredor.php
@@ -0,0 +1,19 @@
+<?php
+    /** 
+     * Class Corredor
+     * 
+     * Clase creada para instaciar objetos de esta clase
+    */
+    class Corredor{
+        public $nombre;
+        public $apellidos;
+        public $ciudad;
+        public $fechaNacimiento;
+        public $sexo;
+        public $email;
+        public $dni;
+        public $edad;
+        public $id_categoria;
+        public $id_club;
+    }
+?>
---
+++ tema-05/proyectos/03 - maratoon/config/db.php
@@ -0,0 +1,8 @@
+<?php
+    /* 
+     * db.php
+     * 
+     * Definimos una serie de constantes, empleadas para ser invocadas dentro del proyecto 
+    */
+    DEFINE('HOST','localhost');
+?>
