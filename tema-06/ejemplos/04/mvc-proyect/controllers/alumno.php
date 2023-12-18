d863925139848e6e24f15e4ed822d030e94ebba5
Denthor99 <darancuga@hotmail.com>
2023-12-18 15:01:41 +0100

    Avance Tema 6

---
+++ tema-06/ejemplos/04/mvc-proyect/controllers/alumno.php
@@ -0,0 +1,43 @@
+<?php
+    // Todos los controladores se crean a partir del controlador
+    class Alumno Extends Controller {
+
+        function __construct() {
+            
+            parent ::__construct();
+            
+            
+        }
+        // Render se ejecuta por defecto, es decir, se ejecuta cuando en la URL no hay un segundo parametro
+        function render() {
+            // Creo la propiedad alumnos dentro de la vista del modelo asignado al controlador, y ejecuto el modelo get(); 
+            $this->view->alumnos = $this->model->get();
+            
+            // Cargara la vista alumno
+            $this->view->render('alumno/main/index');
+        }
+
+        /**
+         * Método new
+         * Creamos un nuevo alumno
+         */
+        function new(){
+            // Etiqueta titulo
+            $this->view->title = "Añadir - Gestión Alumnos";
+
+            // Obtenemos los cursos para la lista dinamica
+            $this->view->cursos = $this->model->getCursos();
+
+            // Cargamos la vista con el formulario
+            $this->view->render('alumno/new/index');
+        }
+        /**
+         * Método show
+         * Muestra los detalles de un registro
+         */
+        function show($param = []){
+
+        }
+    }
+
+?>
